26f148caae23f450efca8aab5159a6e131e27782
ikuniewiczWaybetter <i.kuniewicz@waybetter.pl>
2026-07-18 22:20:23 +0200

    create popular projects section

--- frontend/app/pages/index.vue
+++ frontend/app/pages/index.vue
@@ -3,16 +3,22 @@ import { useHousePlanService } from '~/composables/services/useHousePlanService'
 
 const housePlanService = useHousePlanService()
 
-const { data } = await useAsyncData('featured-house-plans', () =>
+const { data: featuredData } = await useAsyncData('featured-house-plans', () =>
   housePlanService.listHousePlans({ limit: 6 })
 )
+const featuredPlans = computed(() => featuredData.value?.data ?? [])
 
-const featuredPlans = computed(() => data.value?.data ?? [])
+const { data: frequentlySelectedData } = await useAsyncData(
+  'frequently-selected-house-plans',
+  () => housePlanService.listHousePlans({ limit: 10 })
+)
+const frequentlySelectedPlans = computed(
+  () => frequentlySelectedData.value?.data ?? []
+)
 
 const { data: popularData } = await useAsyncData('popular-house-plans', () =>
   housePlanService.listHousePlans({ limit: 10 })
 )
-
 const popularPlans = computed(() => popularData.value?.data ?? [])
 </script>
 
@@ -27,8 +33,21 @@ const popularPlans = computed(() => popularData.value?.data ?? [])
     <StyleBanner />
 
     <CardCarousel
-      :items="popularPlans"
+      :items="frequentlySelectedPlans"
       title="Najczęściej wybierane projekty"
+    >
+      <template #default="{ item, index }">
+        <HousePlanCard
+          :plan="item"
+          :hot-promo="index % 2 === 0"
+          class="snap-start shrink-0 w-[279px]"
+        />
+      </template>
+    </CardCarousel>
+
+    <CardCarousel
+      :items="popularPlans"
+      title="Popularne projekty"
     >
       <template #default="{ item, index }">
         <HousePlanCard
