4b337f9b0dd5b01ba2242d824efacf6dd0cdcd81
ikuniewiczWaybetter <i.kuniewicz@waybetter.pl>
2026-07-20 22:41:31 +0200

    improve newsletter responsivness

--- frontend/app/components/NewsletterSection.vue
+++ frontend/app/components/NewsletterSection.vue
@@ -28,16 +28,16 @@ const SendWebhook = async () => {
 <template>
   <section class="w-full max-w-[1324px] mx-auto md:px-0 px-2 py-8">
     <div
-      class="relative rounded-lg bg-brand-blue-700 shadow-sm ring-1 ring-black/10 overflow-hidden h-[846px] md:h-[350px]"
+      class="relative rounded-lg bg-brand-blue-700 shadow-sm ring-1 ring-black/10 overflow-hidden lg:h-[350px]"
     >
       <NuxtImg
         src="/imgs/newsletter.png"
         alt="Zapisz się do newslettera i nie przegap okazji"
-        class="absolute object-cover w-full h-[350px] left-0 top-0 md:w-[569px] md:h-full"
+        class="relative lg:absolute object-cover w-full h-[350px] left-0 top-0 lg:w-[569px] lg:h-full"
       />
 
       <div
-        class="absolute md:hidden w-full h-[351px] left-0 top-0"
+        class="absolute lg:hidden w-full h-[351px] left-0 top-0"
         style="
           background: linear-gradient(
             180deg,
@@ -48,7 +48,7 @@ const SendWebhook = async () => {
       />
 
       <div
-        class="hidden md:block absolute left-0 top-0 w-[570px] h-full"
+        class="hidden lg:block absolute left-0 top-0 w-[570px] h-full"
         style="
           background: linear-gradient(
             90deg,
@@ -61,14 +61,14 @@ const SendWebhook = async () => {
       <NuxtImg
         src="/svgs/yellow-mail.svg"
         alt="mail"
-        class="absolute z-10 ml-[39px] mt-[87px] w-[52px] h-[47px] md:ml-[51px] md:mt-[98px] md:w-[77px] md:h-[86px]"
+        class="absolute z-10 left-[39px] top-[87px] w-[52px] h-[47px] lg:left-[51px] lg:top-[98px] lg:w-[77px] lg:h-[86px]"
       />
 
       <div
-        class="relative md:absolute md:h-full md:w-full md:pl-[448px] lg:pr-[112px] pl-[24px] pr-[24px] mt-[350px] z-20 lg:pt-0 md:mt-[0px] pt-[17px]"
+        class="relative lg:absolute lg:h-full lg:w-full lg:pl-[448px] lg:pr-[112px] pl-[24px] pr-[24px] z-20 lg:pt-0 pt-[17px] pb-10 lg:pb-0"
       >
         <div
-          class="flex items-stretch gap-4 mb-5 lg:mt-[15px] xl:mt-[63px] md:mt-[10px]"
+          class="flex items-stretch gap-4 mb-5 lg:mt-[15px] xl:mt-[63px]"
         >
           <h3
             class="text-white text-2xl lg:text-3xl font-bold align-self-stretch"
@@ -78,7 +78,7 @@ const SendWebhook = async () => {
         </div>
 
         <div
-          class="flex flex-col md:flex-row md:flex-wrap items-stretch gap-4 md:gap-6 text-white/90 mb-6"
+          class="flex flex-col lg:flex-row lg:flex-wrap items-stretch gap-4 lg:gap-6 text-white/90 mb-6"
         >
           <div class="flex items-stretch gap-2">
             <NuxtImg
@@ -109,7 +109,7 @@ const SendWebhook = async () => {
         </div>
 
         <div
-          class="flex flex-col md:flex-row gap-4 md:gap-3 items-stretch w-full"
+          class="flex flex-col lg:flex-row gap-4 lg:gap-3 items-stretch w-full"
         >
           <input
             v-model="email"
@@ -118,7 +118,7 @@ const SendWebhook = async () => {
             class="flex-1 rounded-lg bg-white px-4 py-3 text-brand-blue-700 placeholder-brand-blue-200 focus:outline-none focus:ring-2 focus:ring-orange-500 h-[60px]"
           />
           <button
-            class="rounded-lg bg-brand-green-500 text-white text-base w-full md:max-w-[204px] py-3 font-semibold cursor-pointer transition-transform transform hover:scale-104"
+            class="rounded-lg bg-brand-green-500 text-white text-base w-full lg:max-w-[204px] py-3 font-semibold cursor-pointer transition-transform transform hover:scale-104"
             @click="SendWebhook"
           >
             Zapisz się
@@ -132,7 +132,7 @@ const SendWebhook = async () => {
         </p>
         <NuxtLink to="https://dom.pl/polityka-prywatnosci.pdf" external>
           <p
-            class="mt-[40px] md:mt-4 text-xs font-semibold text-white align-self-stretch md:mr-10 md:mb-[63px]"
+            class="mt-[40px] lg:mt-4 text-xs font-semibold text-white align-self-stretch lg:mr-10 lg:mb-[63px]"
           >
             Administratorem danych, które wpiszesz, będzie DOM.PL z siedzibą
             przy ul. Gen. Gustawa Orlicz-Dreszera 5/lok. 6, 15-797 Białystok.
